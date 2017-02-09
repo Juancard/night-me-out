bcc917e29b313ff62edaf6dfdec27e6a24c1c7ec
juan <juancruz.23@hotmail.com>
2017-02-09 00:15:59 -0300

    makes request to yelp API and gets reponse

---
+++ app/controllers/loadBars.client.js
@@ -0,0 +1,38 @@
+'use strict';
+
+(function () {
+  /*
+  let scriptBarInfoTemplate = document.getElementById('barInfoTemplate');
+
+  let barsHardCoded = {
+    bars: [{
+      name: "lalola",
+      img: "http://lorempixel.com/250/140/food"
+    },{
+      name: "el re bar",
+      img: "http://lorempixel.com/250/140/food"
+    }]
+  }
+  ajaxFunctions.ready(function() {
+    let jadeBarInfoTemplate = scriptBarInfoTemplate.innerHTML;
+    let htmlBarInfoTemplate = jade.compile(jadeBarInfoTemplate)(barsHardCoded);
+    console.log(htmlBarInfoTemplate);
+  });
+  */
+  let btnSearchForBars = document.getElementById('searchBars');
+  let barsFound = document.getElementById('barsFound');
+  let inputToSearch = document.getElementById('toSearch');
+
+  var searchForBars = (event) => {
+    event.preventDefault();
+    let query = inputToSearch.value;
+    let url = "/search?q="+query;
+    console.log(url);
+    ajaxFunctions.ajaxRequest('GET', url, null, (data) => {
+      barsFound.innerHTML = data;
+    })
+  }
+  btnSearchForBars.addEventListener('click', searchForBars);
+})();
+
+//jade.compile($("#jadehi").html())(djson);
--- app/routes/night_bars.js
+++ app/routes/night_bars.js
@@ -2,9 +2,33 @@
 
 module.exports = function (app, appEnv) {
 
+  let YelpHandler = require(appEnv.path + '/app/controllers/yelpHandler.server.js');
+  let yelpHandler = new YelpHandler();
+
   app.route('/')
 			.get(function (req, res) {
 				res.render(appEnv.path + '/app/views/index.pug');
 			});
-
+  app.route('/search')
+    .get((req, res) => {
+      let parameters = {
+        location: req.query.q,
+        sort: '2'
+      }
+      yelpHandler.request(parameters, function(err, response, body){
+        let barsJson = JSON.parse(body);
+        res.render(appEnv.path + '/app/views/bars.pug', barsJson);
+      });
+    });
+  app.route('/api/search')
+    .get((req, res) => {
+      let parameters = {
+        location: req.query.q,
+        sort: '2'
+      }
+      console.log(parameters);
+      yelpHandler.request(parameters, function(err, response, body){
+        res.json(JSON.parse(body));
+      });
+    });
 }
