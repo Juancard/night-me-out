800501170a63dc317208548ede7a77ad526b6ef2
juan <juancruz.23@hotmail.com>
2017-02-11 11:10:57 -0300

    refactoring on calling yelp api

--- app/controllers/yelpHandler.server.js
+++ app/controllers/yelpHandler.server.js
@@ -12,6 +12,10 @@ let _ = require('lodash');
 let Bar = require('../models/bars.js');
 
 function yelpHandler() {
+  this.searchUrl = 'http://api.yelp.com/v2/search',
+  this.businessUrl = 'http://api.yelp.com/v2/business/',
+  this.consumerSecret = process.env.YELP_SECRET,
+  this.tokenSecret = process.env.YELP_TOKEN_SECRET,
   /* Function for yelp call
    * ------------------------
    * set_parameters: object with params to search
@@ -21,44 +25,7 @@ function yelpHandler() {
     /* The type of request */
     let httpMethod = 'GET';
 
-    /* The url we are using for the request */
-    let url = 'http://api.yelp.com/v2/search';
-
-    /* We can setup default parameters here */
-    let default_parameters = {
-      location: 'San+Francisco',
-      sort: '2'
-    };
-
-    /* We set the require parameters here */
-    let required_parameters = {
-      oauth_consumer_key : process.env.YELP_KEY,
-      oauth_token : process.env.YELP_TOKEN,
-      oauth_nonce : n(),
-      oauth_timestamp : n().toString().substr(0,10),
-      oauth_signature_method : 'HMAC-SHA1',
-      oauth_version : '1.0'
-    };
-
-    /* We combine all the parameters in order of importance */
-    let parameters = _.assign(default_parameters, set_parameters, required_parameters);
-
-    /* We set our secrets here */
-    let consumerSecret = process.env.YELP_SECRET;
-    let tokenSecret = process.env.YELP_TOKEN_SECRET;
-
-    /* Then we call Yelp's Oauth 1.0a server, and it returns a signature */
-    /* Note: This signature is only good for 300 seconds after the oauth_timestamp */
-    let signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
-
-    /* We add the signature to the list of paramters */
-    parameters.oauth_signature = signature;
-
-    /* Then we turn the paramters object, to a query string */
-    let paramURL = qs.stringify(parameters);
-
-    /* Add the query string to the url */
-    let apiURL = url+'?'+paramURL;
+    let apiURL = this.makeApiUrl(httpMethod, this.searchUrl, set_parameters);
 
     /* Then we use request to send make the API Request */
     request(apiURL, function(error, response, body){
@@ -70,9 +37,16 @@ function yelpHandler() {
     let httpMethod = 'GET';
 
     /* The url we are using for the request */
-    let url = 'http://api.yelp.com/v2/business/' + businessId;
+    let url = this.businessUrl + businessId;
 
-    /* We set the require parameters here */
+    let apiURL = this.makeApiUrl(httpMethod, url);
+
+    /* Then we use request to send make the API Request */
+    request(apiURL, function(error, response, body){
+      return callback(error, response, body);
+    });
+  },
+  this.makeApiUrl = (httpMethod, url, set_parameters=null) => {
     let required_parameters = {
       oauth_consumer_key : process.env.YELP_KEY,
       oauth_token : process.env.YELP_TOKEN,
@@ -83,15 +57,18 @@ function yelpHandler() {
     };
 
     /* We combine all the parameters in order of importance */
-    let parameters = _.assign(required_parameters);
-
-    /* We set our secrets here */
-    let consumerSecret = process.env.YELP_SECRET;
-    let tokenSecret = process.env.YELP_TOKEN_SECRET;
+    let parameters;
+    if (set_parameters){
+      parameters = _.assign(set_parameters, required_parameters);
+    } else {
+      parameters = _.assign(required_parameters);
+    }
 
     /* Then we call Yelp's Oauth 1.0a server, and it returns a signature */
     /* Note: This signature is only good for 300 seconds after the oauth_timestamp */
-    let signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
+    let signature = oauthSignature.generate(httpMethod, url,
+      parameters, this.consumerSecret, this.tokenSecret,
+      { encodeSignature: false});
 
     /* We add the signature to the list of paramters */
     parameters.oauth_signature = signature;
@@ -101,11 +78,8 @@ function yelpHandler() {
 
     /* Add the query string to the url */
     let apiURL = url+'?'+paramURL;
-    console.log(apiURL);
-    /* Then we use request to send make the API Request */
-    request(apiURL, function(error, response, body){
-      return callback(error, response, body);
-    });
+
+    return apiURL;
   }
 };
 
